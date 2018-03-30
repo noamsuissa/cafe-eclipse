a5c356cf6804b4f13b04a881840ece6778c601d5
lbelli2 <lucasbellido98@hotmail.com>
2018-03-30 19:10:46 -0400

    problem fixed

--- src/View/Main.java
+++ src/View/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("LogInScreenController.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("LogInScreenView.fxml"));
         primaryStage.setTitle("RestoApp Iteration 3");
         primaryStage.setScene(new Scene(root, 800, 800));
         primaryStage.setResizable(false);
