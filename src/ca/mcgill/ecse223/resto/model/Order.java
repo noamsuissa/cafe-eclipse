c7158f4e9f3b638e87748206d970e438276084bd
Noam Suissa <noam.suissa01@gmail.com>
2018-04-03 17:29:45 -0400

    test

--- src/ca/mcgill/ecse223/resto/model/Order.java
+++ src/ca/mcgill/ecse223/resto/model/Order.java
@@ -1,5 +1,5 @@
 /*PLEASE DO NOT EDIT THIS CODE*/
-/*This code was generated using the UMPLE 1.27.0.3728.d139ed893 modeling language!*/
+/*This code was generated using the UMPLE 1.26.1-f40f105-3613 modeling language!*/
 
 package ca.mcgill.ecse223.resto.model;
 import java.io.Serializable;
@@ -7,7 +7,7 @@ import java.sql.Date;
 import java.sql.Time;
 import java.util.*;
 
-// line 49 "../../../../../RestoAppPersistence.ump"
+// line 48 "../../../../../RestoAppPersistence.ump"
 // line 60 "../../../../../RestoApp v3.ump"
 public class Order implements Serializable
 {
@@ -341,7 +341,7 @@ public class Order implements Serializable
   {
     return 0;
   }
-  /* Code from template association_AddManyToOne */
+
   public OrderItem addOrderItem(int aQuantity, PricedMenuItem aPricedMenuItem, Seat... allSeats)
   {
     return new OrderItem(aQuantity, aPricedMenuItem, this, allSeats);
@@ -451,7 +451,7 @@ public class Order implements Serializable
   {
     return 0;
   }
-  /* Code from template association_AddManyToOne */
+
   public Bill addBill(RestoApp aRestoApp, Seat... allIssuedForSeats)
   {
     return new Bill(this, aRestoApp, allIssuedForSeats);
@@ -536,16 +536,10 @@ public class Order implements Serializable
     
     Waiter placeholderWaiter = waiter;
     this.waiter = null;
-    if(placeholderWaiter != null)
-    {
-      placeholderWaiter.removeOrder(this);
-    }
+    placeholderWaiter.removeOrder(this);
     RestoApp placeholderRestoApp = restoApp;
     this.restoApp = null;
-    if(placeholderRestoApp != null)
-    {
-      placeholderRestoApp.removeOrder(this);
-    }
+    placeholderRestoApp.removeOrder(this);
     for(int i=bills.size(); i > 0; i--)
     {
       Bill aBill = bills.get(i - 1);
@@ -553,7 +547,7 @@ public class Order implements Serializable
     }
   }
 
-  // line 54 "../../../../../RestoAppPersistence.ump"
+  // line 53 "../../../../../RestoAppPersistence.ump"
    public static  void reinitializeAutouniqueNumber(List<Order> orders){
     for (Order order : orders) {
       if (order.getNumber() > nextNumber) {
@@ -577,7 +571,7 @@ public class Order implements Serializable
   // DEVELOPER CODE - PROVIDED AS-IS
   //------------------------
   
-  // line 52 "../../../../../RestoAppPersistence.ump"
+  // line 51 ../../../../../RestoAppPersistence.ump
   private static final long serialVersionUID = -3900912597282882073L ;
 
   
